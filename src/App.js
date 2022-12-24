94f6c58e79e06b7ce685d6b3d2ffd2406c9b7e76
Sean Carlyle <808jalapeno@gmail.com>
2022-12-24 01:18:31 -1000

    adding pokedex files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import axios from "axios"
 
 function App() {
+  const [pokemon, setPokemon] = useState("pikachu");
+  const [pokemonData, setPokemonData] = useState([]);
+  const [pokemonType, setPokemonType] = useState("");
+
+  const getPokemon = async () => {
+    const toArray = [];
+    try {
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`; 
+      const res = await axios.get(url); // Grab const from URL
+      toArray.push(res.data)
+      setPokemonType(res.data.types[0].type.name) // Set type of pokemon
+      setPokemonData(toArray)
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleChange = (e) => {
+    setPokemon(e.target.value.toLowerCase())
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    getPokemon();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit ={handleSubmit}>
+        <label>
+          <input type ="text" onChange={handleChange} placeholder= "Enter Pokemon Name">
+          </input>
+        </label>
+      </form>
+      {pokemonData.map((data) => {
+        return(
+          <div className='container'>
+            <img />
+            <div className='divTable'>
+              <div className='divTableBody'></div>
+              <div className='divTableRow'>
+                <div className='divTableCell'>Type</div>
+                <div className='divTableCell'>{pokemonType}</div>
+              </div>
+              <div className='divTableRow'>
+                <div className='divTableCell'>Height</div>
+                <div className='divTableCell'>{" "}{Math.round(data.height * 3.9)} "</div> 
+              </div>
+              <div className='divTableRow'>
+                <div className='divTableCell'></div>
+                <div className='divTableCell'>{pokemonType}</div>
+              </div>
+              <div className='divTableRow'>
+                <div className='divTableCell'>Type</div>
+                <div className='divTableCell'>{pokemonType}</div>
+              </div>
+            </div>
+          </div>
+        )
+      })}
     </div>
   );
 }
