501777cf0cee166d642d16299a6e660c69c6bdd3
Sean Carlyle <808jalapeno@gmail.com>
2023-04-03 12:55:08 -0700

    test

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [pokemon, setPokemon] = useState("pikachu");
   const [pokemonData, setPokemonData] = useState([]);
   const [pokemonType, setPokemonType] = useState("");
+  const [pokemonTypeTwo, setPokemonTypeTwo] = useState("None");
 
   const getPokemon = async () => {
     const toArray = [];
@@ -14,6 +15,7 @@ function App() {
       const res = await axios.get(url); // Grab const from URL
       toArray.push(res.data)
       setPokemonType(res.data.types[0].type.name) // Set type of pokemon
+      //setPokemonTypeTwo(res.data.types[1].type.name) // Set type of pokemon
       setPokemonData(toArray)
       console.log(res);
     } catch (e) {
@@ -47,18 +49,22 @@ function App() {
                 <div className='divTableRow'>
                   <div className='divTableCell'>Type</div>
                   <div className='divTableCell'>{pokemonType}</div>
+                  <div className='divTableCell'>{pokemonType}</div>
                 </div>
                 <div className='divTableRow'>
                   <div className='divTableCell'>Height</div>
                   <div className='divTableCell'>{" "}{Math.round(data.height * 3.9)} "</div> 
+                  <div className='divTableCell'>{pokemonType}</div>
                 </div>
                 <div className='divTableRow'>
                   <div className='divTableCell'>Weight</div>
                   <div className='divTableCell'>{" "}{Math.round(data.weight / 4.3)} lbs</div>
+                  <div className='divTableCell'>{pokemonType}</div>
                 </div>
                 <div className='divTableRow'>
                   <div className='divTableCell'>Placeholder</div>
                   <div className='divTableCell'>{data.game_indices.length}</div>
+                  <div className='divTableCell'>{pokemonType}</div>
               </div>
               </div>
             </div>
